1b91167e99a312da0c28efaf7f7ea59f7a7e771f
ibrahim akil <brhimakil.1@gmail.com>
2025-10-06 00:12:24 +0300

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./css/bootstrap.min.css";
-import "./css/slick.css";
-import "./css/nice-select.css";
-import "./css/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/webfont/css/all.min.css";
+import ".//css/slick.css";
+import "./sass/style.scss";
 import "./css/responsive.css";
 import "./css/custom-colors.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// AGGRESSIVE CACHE CLEAR - Clear ALL localStorage on version mismatch
-const CACHE_VERSION = 'v3'; // Increment this whenever you deploy
-const currentVersion = localStorage.getItem('cache_version');
+import App from "./App.jsx";
 
-if (currentVersion !== CACHE_VERSION) {
-  console.log('Clearing all localStorage cache...');
-  localStorage.clear(); // Clear EVERYTHING
-  localStorage.setItem('cache_version', CACHE_VERSION);
-  console.log('Cache cleared successfully');
-}
-
-// Add global error handler for unhandled promise rejections
-window.addEventListener('unhandledrejection', (event) => {
-  console.error('Unhandled promise rejection:', event.reason);
-  event.preventDefault(); // Prevent page from hanging
-});
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
